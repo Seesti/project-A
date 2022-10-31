2ed13786d4bb24df1e2c4e0af02fdddcc2a1c8f2
Seesti <113047119+Seesti@users.noreply.github.com>
2022-10-31 15:41:31 +0200

    js added

--- index.js
+++ index.js
@@ -1,36 +1,54 @@
 const api_url =  "https://www.omdbapi.com/?apikey=15f80a04&s=fast"
 
 
-
-
-function renderMovies() {
+async function getData(filter) {
     const moviesWrapper = document.querySelector(".movies");
-
-    const movies = getData();
-    console.log(movies)
-    moviesWrapper.innerHTML = 
-`<div class="movie">
-    <figure class="movie__img--wrapper">
-        <img src="./assets/ff7_movie.jpg" alt="" class="movie__img">
-    </figure>
-        <h4 class="movie__name">Fast & Furious 7</h4>
-        <ul class="movie__info-list">
-        <li class="movie__info">director</li>
-        <li class="movie__info">Release year</li>
-    </ul>
-</div>`
-};
-
-setTimeout(() => {
-    renderMovies();
-})
-
-async function getData() {
+            
     const response = await fetch(api_url);
     const data = await response.json();
+
+    const movies = data.Search
+
+    if (filter === "OLDEST_TO_LATEST") {
+        movies.sort((a, b) => parseFloat(a.Year) - parseFloat(b.Year));
+    }
+    else if (filter === "LATEST_TO_OLDEST") {
+        movies.sort((a, b) => parseFloat(b.Year) - parseFloat(a.Year));
+    }
+
+    const moviesHtml = movies.map(movie => {
+                return `<div class="movie">
+            <figure class="movie__img--wrapper">
+               <img src="${movie.Poster}" alt="" class="movie__img">
+           </figure>
+               <h4 class="movie__name">${movie.Title}</h4>
+               <ul class="movie__info-list">
+               <li class="movie__info">director</li>
+               <li class="movie__info">${movie.Year}</li>
+           </ul>
+       </div>`
+ }).join("")
+
+moviesWrapper.innerHTML = moviesHtml
 };
-
-
 
 getData();
 
+function filterMovies(event) {
+        getData(event.target.value);
+}
+        
+        
+        
+        
+        
+        
+
+
+
+// fetch(api_url).then(response => response.json().then(data => {
+//     console.log(data)
+//     moviesWrapper.innerHTML = data.Title
+//     console.log(data)
+
+// }))
